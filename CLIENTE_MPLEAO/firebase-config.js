b255dd12bd616ef27a37e34fad68a7950c95566a
Zion Tecnologia e Sistemas <ziontecsys@gmail.com>
2026-03-27 17:46:49 -0300

    Add files via upload

---
+++ CLIENTE_MPLEAO/firebase-config.js
@@ -0,0 +1,15 @@
+// ==========================================
+// firebase-config.js
+// Configuração central do Firebase
+// Para trocar de projeto: altere apenas este arquivo
+// ==========================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSwblxyauqKlhhIXw3MZsQ9r9dXkRF46s",
+  authDomain: "cliente-mp-leao.firebaseapp.com",
+  projectId: "cliente-mp-leao",
+  storageBucket: "cliente-mp-leao.firebasestorage.app",
+  messagingSenderId: "1024418172536",
+  appId: "1:1024418172536:web:fecfc98a731a2f56a5678d",
+  measurementId: "G-L0C2GYGF46"
+};
